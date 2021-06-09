573f6cda536c81745c28944de3d2bee10000b2f1
Lindsay Lopez <lopez95@berkeley.edu>
2021-06-08 19:03:52 -0700

    displays gameboard

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const gameBoard = (function () {
-  let gameBoardArray = Array(9).fill(null);
+  // let gameBoardArray = Array(9).fill(null);
+  let gameBoardArray = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+  
+  const display = () => {
+    gameBoardArray.forEach((gamepiece, index) => {
+      let gamepieceElement = document.createElement('button');
+      gamepieceElement.dataset.index = index;
+      gamepieceElement.classList.add('gamepiece');
+      gamepieceElement.innerHTML = gamepiece;
+      displayController.gameBoard.appendChild(gamepieceElement);
+    });
+  };
+
   return {
-    gameBoardArray,
+    display,
   };
 })();
 
 const displayController = (function () {
+  let gameBoard = document.querySelector('.gameboard');
   return {
-
+    gameBoard,
   };
 })();
 
-const Player = (name) => {
+const Player = name => {
   return {
 
   };
 };
+
+gameBoard.display();
