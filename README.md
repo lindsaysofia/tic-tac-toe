0aafdab06a14302abfbceb112946dfc6d5fd6be2
Lindsay Lopez <lopez95@berkeley.edu>
2021-06-08 21:58:05 -0700

    Can now place Xs and Os, but no addtl logic to stop game

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ From The Odin Project
 
 1. Store the gameboard as an array inside of a Gameboard object. Players are also going to be stored in object. Also need an object to control the flow of the game itself.
   - Have as little global code as possible. Use modules or a factory.
+  - I created a module for the gameBoard and for the displayController. Used a Factory for the players since there will be more than 1.
+
+2. Set up your HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage (for now you can just manually fill in the array with "X"s and "O"s).
+  - I created the display function within the gameBoard module which will loops over the gameBoardArray and displays its contents.
+  - I also added a bit of stylying to make it look more like a tic-tac-toe game :)
+
+3. Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-const gameBoard = (function () {
-  // let gameBoardArray = Array(9).fill(null);
-  let gameBoardArray = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+const X = 'X';
+const O = 'O';
+
+const game = (function () {
+  let player1Turn = true;
+  
+  let gameboardContainer = document.querySelector('.gameboard');
+
+  const handleGameboardSelection = e => {
+    let index = e.target.dataset.index;
+    let selection = player1Turn ? X : O;
+    let successfulPlacement = gameboard.setGamepiece(index, selection);
+    if (successfulPlacement) {
+      player1Turn = !player1Turn
+    }
+  }
+
+  gameboardContainer.addEventListener('click', handleGameboardSelection);
+
+  return {
+    gameboardContainer,
+  };
+})();
+
+const gameboard = (function () {
+  let gameboardArray = Array(9).fill(null);
   
   const display = () => {
-    gameBoardArray.forEach((gamepiece, index) => {
+    game.gameboardContainer.innerHTML = '';
+    gameboardArray.forEach((gamepiece, index) => {
       let gamepieceElement = document.createElement('button');
       gamepieceElement.dataset.index = index;
       gamepieceElement.classList.add('gamepiece');
       gamepieceElement.innerHTML = gamepiece;
-      displayController.gameBoard.appendChild(gamepieceElement);
+      game.gameboardContainer.appendChild(gamepieceElement);
     });
+  };
+
+  const setGamepiece = (index, selection) => {
+    if (gameboardArray[index] !== null) return false;
+    gameboardArray[index] = selection;
+    display();
+    return true;
   };
 
   return {
     display,
-  };
-})();
-
-const displayController = (function () {
-  let gameBoard = document.querySelector('.gameboard');
-  return {
-    gameBoard,
+    setGamepiece,
   };
 })();
 
 const Player = name => {
+  name = name.toUpperCase();
   return {
 
   };
 };
 
-gameBoard.display();
+gameboard.display();
+let playerX = Player(X);
+let playerO = Player(O);
